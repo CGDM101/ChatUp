d95a23491b755deea99e6006cc0fcbfbcae9aa2c
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-10-05 21:10:22 +0200

    Added code for all switch cases

--- ChatUp/Program.cs
+++ ChatUp/Program.cs
@@ -12,12 +12,13 @@ namespace ChatUp
             bool done = false;
             while (!done)
             {
-                Console.WriteLine("Vad vill du göra? Välj ett alternativ!\n1. Lägga till person\n2. Visa person\n3. Ändra person\n4. Ta bort person\n5a. Lista alla personer\n5b. Lista person vars namn börjar på viss bokstav");
-                string userInput = Console.ReadLine();
+                Console.WriteLine();
+                Console.WriteLine("Vad vill du göra? Välj ett alternativ!\n1. Lägga till person\n2. Visa person\n3. Ändra person\n4. Ta bort person\n5a. Lista alla personer\n5b. Lista person vars namn börjar på viss bokstav\nq. AVSLUTA");
+                string userInput = Console.ReadLine().ToLower().Trim();
                 switch (userInput)
                 {
                     case "1":
-                        Console.Write("Skriv in personens förnamn: ");
+                        Console.Write("Skriv in personens förnamn: "); 
                         string f = Console.ReadLine();
                         Console.Write("Skriv in personens efternamn: ");
                         string l = Console.ReadLine();
@@ -25,17 +26,30 @@ namespace ChatUp
                         newPerson.FirstName = f;
                         newPerson.LastName = l;
                         personList.Add(newPerson);
-
+                        Console.WriteLine(newPerson.FirstName + " tillagd");
+                        //foreach (var item in personList)
+                        //{
+                        //    Console.WriteLine(item.FirstName);
+                        //}
                         //AddPerson();
                         break;
                     case "2":
-                        Console.WriteLine("Vilken av dessa vill du visa? Välj förstabokstav");
+                        Console.WriteLine("Vilken av dessa vill du visa all information om? Välj förstabokstav");
                         string s = Console.ReadLine();
                         foreach (var item in personList)
                         {
                             if ((item.FirstName).Substring(0,1) == s)
                             {
-                                Console.WriteLine(item.FirstName);
+                                Console.WriteLine(item.FirstName); // ska komma från ctor
+                                Console.WriteLine(item.LastName);
+                                Console.WriteLine(item.Alias); // null
+                                Console.WriteLine(item.EmailAddress);
+                                Console.WriteLine(item.FacebookProfile);
+                                Console.WriteLine(item.InstagramProfile);
+                                Console.WriteLine(item.IsBlocked); 
+                                Console.WriteLine(item.IsGhosted);
+                                Console.WriteLine(item.LinkedinProfile);
+                                Console.WriteLine(item.TwitterProfile);
                             }
                             else
                             {
@@ -44,7 +58,7 @@ namespace ChatUp
                         }
                         //ShowPerson();
                         break;
-                    case "3":
+                    case "3": // Ändra person
                         Console.WriteLine("Vilken av dessa vill du ändra? Välj förstabokstav");
                         string ss = Console.ReadLine();
                         foreach (var item in personList)
@@ -52,7 +66,16 @@ namespace ChatUp
                             if ((item.FirstName).Substring(0, 1) == ss)
                             {
                                 Console.WriteLine("Vilken av egenskaperna vill du ändra?");
-                                // ...
+                                Console.WriteLine("[F]örnamn");
+                                Console.WriteLine("[E]fternamn");
+                                Console.WriteLine("[A]lias");
+                                Console.WriteLine("[E]mejl-adress");
+                                Console.WriteLine("[F]acebookprofil");
+                                Console.WriteLine("[I]nstagramprofil");
+                                Console.WriteLine("[B]lockerad");
+                                Console.WriteLine("[G]hostad");
+                                Console.WriteLine("[L]inkedInProfil");
+                                Console.WriteLine("[T]witterprofil");
                             }
                             else
                             {
@@ -65,20 +88,26 @@ namespace ChatUp
                     case "4":
                         Console.WriteLine("Vilken av dessa vill du ta bort? Välj förstabokstav");
                         string sss = Console.ReadLine();
-                        foreach (var item in personList)
+                        foreach (var item in personList) // Invalid Operation Exception
                         {
                             if ((item.FirstName).Substring(0, 1) == sss)
                             {
-                                personList.Remove(item);
+                                //personList.Remove(item);
+                                Console.WriteLine(item.FirstName + " kommer att tas bort");
                             }
                             else
                             {
                                 Console.WriteLine("Fanns ingen med den begynnelsebokstaven.");
                             }
+                            personList.Remove(item);
                         }
+
                         //DeletePerson();
                         break;
                     case "5a":
+                        //Person a = new Person() { FirstName = "Adam" };
+                        //Person b = new Person() { FirstName = "Bertil" }; Person c = new Person() { FirstName = "Caesar" }; Person d = new Person() { FirstName = "David" }; Person e = new Person() { FirstName = "Erik" };
+                        //personList.Add(a); personList.Add(b); personList.Add(c); personList.Add(d); personList.Add(e);
                         foreach (var item in personList)
                         {
                             Console.WriteLine(item.FirstName);
@@ -86,13 +115,18 @@ namespace ChatUp
                         //ListAllPersons();
                         break;
                     case "5b":
-                        Console.WriteLine("Vilken av dessa vill du ta visa? Välj förstabokstav");
+                        Console.WriteLine("Lista alla på viss bokstav - Välj förstabokstav");
                         string ssss = Console.ReadLine();
                         foreach (var item in personList)
                         {
                             if ((item.FirstName).Substring(0, 1) == ssss)
                             {
-                                Console.WriteLine(item.FirstName,item.LastName,item.InstagramProfile,item.IsBlocked,item.Alias);
+                                List<Person> specialList = new List<Person>();
+                                specialList.Add(item);
+                                foreach (var itemm in specialList)
+                                {
+                                    Console.WriteLine(itemm.FirstName);
+                                }
                             }
                             else
                             {
@@ -100,6 +134,9 @@ namespace ChatUp
                             }
                         }
                         //ListCertainPerson();
+                        break;
+                    case "q":
+                        done = true;
                         break;
                     default:
                         Console.WriteLine("Ogitligt val. Försök igen!");
